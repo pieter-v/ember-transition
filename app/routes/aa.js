d2a6691691328c277820aef9ae26a77639a4a32e
pieter-v <d1ntpanic@gmail.com>
2019-01-09 13:23:33 +0100

    load router service and upgrade dependencies

--- app/routes/aa.js
+++ app/routes/aa.js
@@ -1,7 +1,16 @@
 import Route from '@ember/routing/route';
-import { later } from '@ember/runloop';
+import {later} from '@ember/runloop';
+import {inject} from '@ember/service';
 
 export default Route.extend({
+  init() {
+    this._super(...arguments);
+    // just to make sure the router service is loaded
+    console.log(this.router.currentURL);
+  },
+
+  router: inject(),
+
   actions: {
     willTransition(transition) {
       transition.abort();
